826d64c9f137e8c0399876dffada90d74ce4039a
Maksymilian Szymański <maksymilianszymanski2@gmail.com>
2024-05-23 17:43:41 +0200

    add toast when user is end rejestration

--- src/features/authentication/useSignin.ts
+++ src/features/authentication/useSignin.ts
@@ -1,6 +1,7 @@
 import { useMutation } from '@tanstack/react-query'
 import { signUp as signUpApi } from '../../services/apiAuth'
 import { useNavigate } from 'react-router-dom'
+import toast from 'react-hot-toast'
 
 export function useSignUp() {
     const navigate = useNavigate()
@@ -27,9 +28,13 @@ export function useSignUp() {
             }),
         onSuccess: (user) => {
             navigate('/login')
+            toast('Link aktywacyjny został wysłany na podany adres email')
         },
         onError: (err) => {
             console.log(err)
+            toast(
+                'Wystąpił problem z rejestracją konta. Prosimy sptóbować ponownie'
+            )
         },
     })
     return { signUp, isLoading }
