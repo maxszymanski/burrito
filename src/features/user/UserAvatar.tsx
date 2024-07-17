afb84fd990d0a5248abe618429ede5bdb5fef68f
Max Szymański <maksymilianszymanski2@gmail.com>
2024-07-17 14:41:44 +0200

    change Link to updateProfile

--- src/features/user/UserAvatar.tsx
+++ src/features/user/UserAvatar.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import { useUser } from '../authentication/useUser'
 
 function UserAvatar() {
@@ -6,13 +7,13 @@ function UserAvatar() {
     const avatar = user?.user_metadata.avatar || '/images/user2.webp'
 
     return (
-        <a href={avatar}>
+        <Link to="/updateProfile">
             <img
                 className="rounded-full outline-2 outline-gray-100 object-cover w-28 h-28 small:h-36 block small:w-36 object-center border border-yellow-500 shadow-[0px_0px_8px_1px] shadow-yellow-600 xl:h-48 xl:w-48 2xl:w-52 2xl:h-52"
                 src={avatar}
                 alt={`avatar użytkownika ${userName}`}
             />
-        </a>
+        </Link>
     )
 }
 
