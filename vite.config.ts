75524d7004c5aab324080aed52ba776a47949364
Max Szymański <maksymilianszymanski2@gmail.com>
2024-07-24 11:48:29 +0200

    repair RactGA

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    build: {
+        terserOptions: {
+            compress: {
+                drop_console: true,
+            },
+        },
+    },
 })
